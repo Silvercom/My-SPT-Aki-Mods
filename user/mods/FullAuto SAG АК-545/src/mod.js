c0507d0d704b354921f4b2ebd954402d94841679
RadmirSilver <radkumir@gmail.com>
2024-07-07 15:38:26 +0500

    Initial commit

---
+++ user/mods/FullAuto SAG АК-545/src/mod.js
@@ -0,0 +1,18 @@
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+class Mod {
+    postDBLoad(container) {
+        const databaseServer = container.resolve("DatabaseServer");
+        const tables = databaseServer.getTables();
+        const sag545_short = tables.templates.items["628b9c37a733087d0d7fe84b"];
+        sag545_short._props.weapFireType.push("fullauto");
+        sag545_short._props.bFirerate = 650;
+        const sag545Long = tables.templates.items["628b5638ad252a16da6dd245"];
+        sag545Long._props.weapFireType.push("fullauto");
+        sag545Long._props.bFirerate = 650;
+    }
+}
+module.exports = { mod: new Mod() };
+//# sourceMappingURL=mod.js.map
